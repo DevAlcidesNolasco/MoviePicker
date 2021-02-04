5f5b80581b066ea05c89e4e5756f5f891f49a6ec
Alvaro Nolasco <alvarocojonudo@gmail.com>
2021-02-03 18:29:03 -0600

    editing logos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,21 +9,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public routes: any[] = [
     {
       route: "home",
-      name:"Home",
+      name: "Home",
       icon: "home"
     },
     {
       route: "movies",
-      name:"Movies",
+      name: "Movies",
       icon: "videocam"
     },
     {
       route: "series",
-      name:"Series",
+      name: "Series",
       icon: "albums"
     },
     {
@@ -41,6 +41,27 @@ export class AppComponent {
     if (this.platform.is("android")) {
       this.initializeApp();
     }
+  }
+
+  ngOnInit = () => {
+
+    window.onscroll = function () { myFunction() };
+
+    // Get the header
+    var header = document.getElementById("myHeader");
+
+    // Get the offset position of the navbar
+    var sticky = header.offsetTop;
+
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+
   }
 
   initializeApp() {
